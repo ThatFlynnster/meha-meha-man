fff1962430df00e3469e7e23b72ee08ae9d03354
ThatFlynnster <thatflynnster26@gmail.com>
2025-12-09 09:38:13 +0900

    height interpolation attempt

--- character/3d/character_3d.gd
+++ character/3d/character_3d.gd
@@ -9,18 +9,56 @@ const AIR_DECELERATION = 0.0
 const JUMP_VELOCITY = 5.72
 const TERMINAL_VELOCITY = 30.0
 
+@onready var _floor_cast := %FloorShapeCast
+
+var _is_on_floor := false
+var _jump_pending := false
 var _direction := Vector3.ZERO
 var _acceleration
 var _deceleration
 
+# Debug variables
+var _debug_spheres: Array[MeshInstance3D] = []
+var _debug_mesh: SphereMesh = SphereMesh.new()
+var _debug_material: StandardMaterial3D = StandardMaterial3D.new()
 
-func _rotate(rotation: float):
-	#rotate_y(rotation)
+func _check_floor() -> bool:
+	for sphere in _debug_spheres:
+		sphere.queue_free()
+	_debug_spheres.clear()
+	
+	if not _floor_cast.is_colliding(): return false
+	
+	var collision_points = []
+	for i in range(_floor_cast.get_collision_count()):
+		var point = _floor_cast.get_collision_point(i)
+		collision_points.append(point)
+		
+		var debug_sphere = MeshInstance3D.new()
+		debug_sphere.mesh = _debug_mesh
+		add_child(debug_sphere)
+		debug_sphere.top_level = true
+		debug_sphere.physics_interpolation_mode = Node.PHYSICS_INTERPOLATION_MODE_OFF
+		debug_sphere.global_position = point
+		_debug_spheres.append(debug_sphere)
+	
+	var highest_collision_point = collision_points[0]
+	for collision_point in collision_points:
+		if collision_point.y > highest_collision_point.y:
+			highest_collision_point = collision_point
+	
+	var collision_point_height = highest_collision_point.y
+	if position.y - collision_point_height <= 0.0:
+		return true
+	return false
+
+
+func _rotate(y_rotation: float):
 	pass
 
 
-func _set_rotation(rotation: float):
-	self.rotation.y = rotation
+func _set_rotation(y_rotation: float):
+	self.rotation.y = y_rotation
 
 
 func _update_direction(direction: Vector3) -> void:
@@ -28,8 +66,8 @@ func _update_direction(direction: Vector3) -> void:
 
 
 func _jump() -> void:
-	if is_on_floor(): # this check fails sometimes
-		floor_snap_length = 0.0
+	if _is_on_floor:
+		_jump_pending = true
 		velocity.y = JUMP_VELOCITY
 
 
@@ -45,31 +83,29 @@ func _ready() -> void:
 	controller.connect("set_rotation", _set_rotation)
 	controller.connect("direction_updated", _update_direction)
 	controller.connect("jump_requested", _jump)
+	
+	# Configure the visual style of the debug sphere
+	_debug_mesh.radius = 0.05  # Size of the sphere
+	_debug_mesh.height = 0.1
+	_debug_material.albedo_color = Color.RED
+	_debug_material.shading_mode = BaseMaterial3D.SHADING_MODE_UNSHADED # Makes it glow/visible in dark
+	_debug_mesh.surface_set_material(0, _debug_material)
 
 
 func _physics_process(delta: float) -> void:
-	# 1. Handle Gravity / Vertical Movement
-	if not is_on_floor():
-		floor_snap_length = 0.0
-		velocity += get_gravity() * delta
-		velocity.y = min(velocity.y, TERMINAL_VELOCITY)
-
-	# 2. Determine Acceleration/Deceleration vars
-	_acceleration = AIR_ACCELERATION
-	_deceleration = AIR_DECELERATION
-
-	if is_on_floor():
-		var floor_normal = get_floor_normal()
-		var slope_angle = floor_normal.angle_to(Vector3.UP)
-		var slope_sine = sin(slope_angle)
-		#print_debug(slope_sine)
-		if slope_sine > 0.0:
-			floor_snap_length = slope_sine * 0.1 * SPEED
-			#floor_snap_length = 0.1
-		else: floor_snap_length = 0.0
-		
+	_is_on_floor = _check_floor()
+	
+	if _is_on_floor:
 		_acceleration = FLOOR_ACCELERATION
 		_deceleration = FLOOR_DECELERATION
+		if not _jump_pending:
+			velocity.y = 0
+	else:
+		_acceleration = AIR_ACCELERATION
+		_deceleration = AIR_DECELERATION
+		_jump_pending = false
+		velocity += get_gravity() * delta
+		velocity.y = min(velocity.y, TERMINAL_VELOCITY)
 
 	# 3. Isolate Horizontal Velocity (Discard Y)
 	var h_velocity = Vector2(velocity.x, velocity.z)
